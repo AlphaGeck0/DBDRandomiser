d855236f00c03854c777f620690e0005684f1e18
Alpha Geck0 <geckoman.streaming@gmail.com>
2025-04-27 15:41:13 +0100

    Updating for first publish

--- DBDRandomiser/App.xaml.cs
+++ DBDRandomiser/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System;
+using System.IO;
 
 namespace DBDRandomiser
 {
@@ -9,6 +11,19 @@ namespace DBDRandomiser
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                File.WriteAllText("error.log", e.ExceptionObject.ToString());
+            };
+
+            this.DispatcherUnhandledException += (sender, e) =>
+            {
+                File.WriteAllText("error.log", e.Exception.ToString());
+                e.Handled = true;
+            };
+        }
     }
 
 }
